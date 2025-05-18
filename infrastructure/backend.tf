51b71105a78672813964720a0ad168fc366f780b
VianeyArteaga <choisoobinmx@gmail.com>
2025-05-18 17:53:29 -0600

    cambio

--- infrastructure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-backend "axurerm" {
-  storage_accoount_name = "generalstorageamin"
-  container_imagen = "webappstate"
-  key = "stateActions.tfatate"
-}
-
-}
--- infrastructure/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-rg_name = "containers-test"
-location = "East US 2"
-containers_image = "mjtaehyung/sitio001:lastest"
